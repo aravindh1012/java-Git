d5323f641597aab745c74ad98db407da0fef937b
Aravindh <vaparavindh7@gmail.com>
2025-05-26 23:47:43 +0530

    String starting

---
+++ StringProblem.java
@@ -0,0 +1,8 @@
+public class StringProblem {
+    public static void main(String[] args){
+        //string pool is separate memory structure inside the heap. (all the similar values of strings are not like recreated the same object in the pool)
+        //immutablity string cannot be changed in java in object(if we need to change the string we need to create a new object)for security purpose
+        
+
+    }
+}
